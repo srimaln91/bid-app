0c4400dd3d015c3f80d9b896d7f44c22cfba87da
H.M.S.Nishantha <srimaln91@gmail.com>
2017-08-20 18:52:39 +0530

    Updated /property api endpoint to respond according to request parameters

--- controllers/property/GetProperty.js
+++ controllers/property/GetProperty.js
@@ -2,13 +2,27 @@ var property = require("../../models/property");
 var errHandler = require("../../modules/ErrorHandler");
 
 var controller = function (req, res, next) {
+  
+  var params = {};
+  if(req.body.propertyId != undefined ){
+    params = { _id: req.body.propertyId };
 
-  property.find({})
-    .populate('bids')
+    property.findOne(params)
+    .populate('bids', null, null, { sort: { 'value': -1 } })
     .exec(function (err, properties) {
       if (err) return handleError(err);
       res.json(properties);
     });
+
+  }
+  else{
+    property.find(params)
+    .populate('bids', null, null, { sort: { 'value': -1 } })
+    .exec(function (err, properties) {
+      if (err) return handleError(err);
+      res.json(properties);
+    });
+  }
 
 };
 
